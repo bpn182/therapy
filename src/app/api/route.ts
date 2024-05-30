6b7c54222ef13723b8c624c81901ab3ec58e327e
Bipin Bhandari <bhandaribpn1996@gmail.com>
2024-05-31 00:01:55 +1000

    removed environment logging

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+
 export async function GET() {
-  return NextResponse.json({ data: "data", env: process.env });
+  return NextResponse.json({ data: "data" });
 }
+ 
